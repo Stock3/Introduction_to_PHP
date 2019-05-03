18c7b6db28858801a1fa1eba99c87d252fc432e5
Alex Mitskan <olexiy.mitskan@gmail.com>
2019-05-03 18:48:13 +0300

    Variables and their types

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $age = 21;
+    echo $age;
+    echo '<br/>';
+
+    $s = "Мій вік $age років";
+    echo $s;
+    echo '<br/>';
+
+    $d = 2.1;
+    echo $d;
+    echo '<br/>';
+
+    $bool = true;
+    $bool_1 = false;
+    echo "true: $bool; false: $bool_1";
+    echo '<br/>';
+
+    $obj = null;
+    echo "NULL: $obj";
+?>
