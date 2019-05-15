6153681eb0949df97c95b0ed816cfb439c3d2a19
Alex Mitskan <olexiy.mitskan@gmail.com>
2019-05-15 18:57:41 +0300

    Operator if-else

--- index.php
+++ index.php
@@ -1,25 +1,39 @@
 <?php
-    $x = 10;
-    $y = 0;
-    $b_1 = $x > 10;
-    $b_2 = $y == 0;
-    echo "x = $x; y = $y<br />";
-    echo "x > 10 = $b_1; (y == 0) = $b_2<br />";
+    $x = 7;
+    if ($x < 10) echo 'x меньше 10';
+    else echo 'x больше или равен 10';
+    echo '<br />';
+    if ($x < 7) echo 'x меньше 7';
+    elseif ($x > 7) echo 'x больше 7';
+    else echo 'x равен 7';
+    echo '<br />';
 
-    $b_1 = true;
-    $b_2 = false;
-    echo "b_1 = $b_1; b_2 = $b_2<br />";
-    echo 'Отрицание b_1: '.!$b_1.'<br />';
-    echo 'b_1 ИЛИ b_2: '.($b_1 || $b_2).'<br />';
-    echo 'b_1 И b_2: '.($b_1 && $b_2).'<br />';
+    $a = 5;
+    $b = 3;
+    if ($a < $b || $b == 3) echo 'Истина';
+    else echo 'Ложь';
 
-    //різні операнди, тоді true
-    echo 'b_1 ИСКЛЮЧАЮЩЕЕ ИЛИ b_2: '.($b_1 xor $b_2).'<br />';
+    /* Следующие строки НЕ безопасны! Однако, на данный момент это не критично. */
 
-    echo (!($b_1 && $b_2) xor (!$b_1 || $x < 10)).'<br />';
+    if (isset($_GET['com'])) $com = $_GET['com'];
+    else $com = false;
 
-    $x = 5;
-    $y = '5';
-    echo 'Результат проверки на равенство x и y '.($x == $y).'<br />';
-    echo 'Результат проверки на эквивалентность x и y '.($x === $y).'<br />';
+    $com = isset($_GET['com'])? $_GET['com'] : false;
+
+    $com = $_GET['com']?? false;
+    echo '<br />';
+    if ($com == 'add') {
+        $summa = $a + $b;
+        echo "$a + $b = $summa";
+    }
+    elseif ($com == 'mult') {
+        $mult = $a * $b;
+        echo "$a * $b = $mult";
+    }
+    elseif ($com) echo 'Неизвестная операция!';
+
 ?>
+<div>
+    <a href="?com=add">Сложить</a>
+    <a href="?com=mult">Перемножить</a>
+</div>
